2d112e417d0ce2826b2e4ede23262013ec1cb094
fabien townsend <ftownsend@8thligth.com>
2016-09-08 17:01:46 +0100

    board set position

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,18 @@ class BoardGame
   attr_reader :board
 
   def initialize
-    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+  end
+
+  def set_mark(mark, position)
+    @board[get_row(position)][get_column(position)] = mark
+  end
+
+  def get_column position
+    position % @board.size
+  end
+
+  def get_row position
+    position / @board.size
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,45 @@
 require 'board'
 
 RSpec.describe BoardGame do
+  before(:each) do
+    @board_game = BoardGame.new
+  end
+
   it "should have an empty board" do
-  board_expected = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  board_game = BoardGame.new
-  expect(board_game.board).to eq(board_expected)
+    board_expected = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+
+    expect(@board_game.board).to eq(board_expected)
+  end
+
+  it "should give the column position" do
+    expect(@board_game.get_column(0)).to eq(0)
+    expect(@board_game.get_column(1)).to eq(1)
+    expect(@board_game.get_column(2)).to eq(2)
+    expect(@board_game.get_column(3)).to eq(0)
+    expect(@board_game.get_column(6)).to eq(0)
+  end
+
+  it "should give the row position" do
+    expect(@board_game.get_row(0)).to eq(0)
+    expect(@board_game.get_row(3)).to eq(1)
+    expect(@board_game.get_row(8)).to eq(2)
+  end
+
+  it "should set the mark at the position 8" do
+    board_expected = [[0, 1, "X"], [3, 4, 5], [6, 7, 8]]
+    @board_game.set_mark("X", 2)
+    expect(@board_game.board).to eq(board_expected)
+  end
+
+  it "should set the mark at the position 4" do
+    board_expected = [[0, 1, 2], [3, "X", 5], [6, 7, 8]]
+    @board_game.set_mark("X", 4)
+    expect(@board_game.board).to eq(board_expected)
+  end
+
+  it "should set the mark at the position 0" do
+    board_expected = [["X", 1, 2], [3, 4, 5], [6, 7, 8]]
+    @board_game.set_mark("X", 0)
+    expect(@board_game.board).to eq(board_expected)
   end
 end
