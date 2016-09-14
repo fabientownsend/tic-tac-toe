079de14cce49ca6049aaae8dce040b6269a9da9e
fabien townsend <fabientownsend@gmail.com>
2016-09-14 08:53:52 +0100

    update coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
-require 'simplecov'
-SimpleCov.start
-
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
