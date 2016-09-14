f67ce4287e2155f8b2e16189224d9cc419b0f75a
fabien townsend <fabientownsend@gmail.com>
2016-09-14 16:32:00 +0100

    test integration mock move

--- spec/game_play_spec.rb
+++ spec/game_play_spec.rb
@@ -1,50 +1,55 @@
 require 'spec_helper'
+
+require 'board'
+require 'cli_interface'
 require 'game_play'
-require 'player'
+require 'human'
 
 RSpec.describe GamePlay do
-  let(:player_one) {Player.new("X", nil)}
-  let(:player_two) {Player.new("O", nil)}
-
-  let(:input) {StringIO.new("1\n")}
-  let(:output) {StringIO.new}
-  let(:interface) {CliInterface.new(input, output)}
-
   let(:board) {Board.new}
 
-  let(:game_play) {GamePlay.new(player_one, player_two, board, interface)}
-
-  it "should be player one the current first player" do
-    expect(game_play.current_player).to eq(player_one)
-  end
-
   it "should finish the game displaying a tie" do
-    board.set_mark("X", 0)
-    board.set_mark("O", 1)
-    board.set_mark("X", 2)
-    board.set_mark("X", 3)
-    board.set_mark("O", 4)
-    board.set_mark("X", 5)
-    board.set_mark("O", 6)
-    board.set_mark("X", 7)
-    board.set_mark("O", 8)
+    input = StringIO.new("0\n1\n2\n4\n3\n6\n5\n8\n7\n")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    player_one = Human.new("X", interface)
+    player_two = Human.new("O", interface)
+
+    board = Board.new
+
+    game_play = GamePlay.new(player_one, player_two, board, interface)
 
     game_play.play
     expect(output.string).to include("It's a tie!\n")
   end
 
   it "should finish the game displaying a tie" do
-    board.set_mark("X", 0)
-    board.set_mark("X", 1)
-    board.set_mark("X", 2)
-    board.set_mark("X", 3)
-    board.set_mark("O", 4)
-    board.set_mark("X", 5)
-    board.set_mark("O", 6)
-    board.set_mark("X", 7)
-    board.set_mark("O", 8)
+    input = StringIO.new("1\n4\n7\n0\n2\n8\n")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    player_one = Human.new("X", interface)
+    player_two = Human.new("O", interface)
+
+    board = Board.new
+
+    game_play = GamePlay.new(player_one, player_two, board, interface)
+    game_play.play
+    expect(output.string).to include("The winner is: X")
+  end
+
+  it "should be win by the player two" do
+    input = StringIO.new("0\n1\n3\n2\n6\n")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    player_one = Human.new("X", interface)
+    player_two = Human.new("O", interface)
+
+    game_play = GamePlay.new(player_one, player_two, board, interface)
 
     game_play.play
-    expect(output.string).to include("The winner is: ")
+    expect(output.string).to include("The winner is: O")
   end
 end
