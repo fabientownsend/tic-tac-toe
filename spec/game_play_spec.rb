e25b5506a2948b12ea92fe55d96a54c57c9bf253
fabien townsend <fabientownsend@gmail.com>
2016-09-15 16:23:57 +0100

    improve test coverage

--- spec/game_play_spec.rb
+++ spec/game_play_spec.rb
@@ -57,6 +57,83 @@ RSpec.describe GamePlay do
     expect(output.string).to include("The winner is: O")
   end
 
+  it "should be win by the player two with the second move already played" do
+    type_game = "1\n"
+    first_player = "2\n"
+
+    moves_part_one = "0\n"
+    already_played = "0\n"
+    moves_part_two = "1\n3\n2\n6\n"
+    input = StringIO.new("#{type_game}#{first_player}#{moves_part_one}#{already_played}#{moves_part_two}")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    game_play = GamePlay.new(Board.new, interface)
+
+    game_play.game_selection
+    game_play.select_first_player
+    game_play.play
+    expect(output.string).to include("The winner is: O")
+  end
+
+  it "should be win by the player two with the second move too hight" do
+    type_game = "1\n"
+    first_player = "2\n"
+
+    moves_part_one = "0\n"
+    too_hight = "100\n"
+    moves_part_two = "1\n3\n2\n6\n"
+    input = StringIO.new("#{type_game}#{first_player}#{moves_part_one}#{too_hight}#{moves_part_two}")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    game_play = GamePlay.new(Board.new, interface)
+
+    game_play.game_selection
+    game_play.select_first_player
+    game_play.play
+    expect(output.string).to include("The winner is: O")
+  end
+
+  it "should be win by the player two with the second move too low" do
+    type_game = "1\n"
+    first_player = "2\n"
+
+    moves_part_one = "0\n"
+    too_low = "-100\n"
+    moves_part_two = "1\n3\n2\n6\n"
+    input = StringIO.new("#{type_game}#{first_player}#{moves_part_one}#{too_low}#{moves_part_two}")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    game_play = GamePlay.new(Board.new, interface)
+
+    game_play.game_selection
+    game_play.select_first_player
+    game_play.play
+    expect(output.string).to include("The winner is: O")
+  end
+
+  it "should be win by the player two with the second invalid" do
+    type_game = "1\n"
+    first_player = "2\n"
+
+    moves_part_one = "0\n"
+    invalid = "fdhasjfjd\n"
+    moves_part_two = "1\n3\n2\n6\n"
+    input = StringIO.new("#{type_game}#{first_player}#{moves_part_one}#{invalid}#{moves_part_two}")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    game_play = GamePlay.new(Board.new, interface)
+
+    game_play.game_selection
+    game_play.select_first_player
+    game_play.play
+    expect(output.string).to include("The winner is: O")
+  end
+
+
   it "should create two human player" do
     game_play = create_type_game("1\n")
     game_play.game_selection
@@ -81,12 +158,36 @@ RSpec.describe GamePlay do
     expect(game_play.player_two).to be_a(Computer)
   end
 
-  it "should create one human and one computer" do
+  it "should not accept the first input but the second" do
     game_play = create_type_game("thisIsNotAValidInput\n3\n")
     game_play.game_selection
 
     expect(game_play.player_one).to be_a(Computer)
     expect(game_play.player_two).to be_a(Computer)
+  end
+
+  it "should not accept the first input which is too low but the second" do
+    game_play = create_type_game("-1\n3\n")
+    game_play.game_selection
+
+    expect(game_play.player_one).to be_a(Computer)
+    expect(game_play.player_two).to be_a(Computer)
+  end
+
+  it "should not accept the first input which is too hight but the second" do
+    game_play = create_type_game("4\n3\n")
+    game_play.game_selection
+
+    expect(game_play.player_one).to be_a(Computer)
+    expect(game_play.player_two).to be_a(Computer)
+  end
+
+  def create_type_game(type)
+    input = StringIO.new(type)
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    GamePlay.new(Board.new, interface)
   end
 
   it "should set player one as first player" do
@@ -119,11 +220,48 @@ RSpec.describe GamePlay do
     expect(game_play.current_player).to eq(game_play.player_one)
   end
 
-  def create_type_game(type)
-    input = StringIO.new(type)
+  it "should not accept the first input which is too low but the second" do
+    type_game = "1\n"
+    first_player = "-1\n1\n"
+    input = StringIO.new("#{type_game}#{first_player}")
     output = StringIO.new
     interface = CliInterface.new(input, output)
 
-    GamePlay.new(Board.new, interface)
+    game_play = GamePlay.new(Board.new, interface)
+
+    game_play.game_selection
+    game_play.select_first_player
+
+    expect(game_play.current_player).to eq(game_play.player_two)
+  end
+
+  it "should not accept the first input which is too hight but the second" do
+    type_game = "1\n"
+    first_player = "5\n1\n"
+    input = StringIO.new("#{type_game}#{first_player}")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    game_play = GamePlay.new(Board.new, interface)
+
+    game_play.game_selection
+    game_play.select_first_player
+
+    expect(game_play.current_player).to eq(game_play.player_two)
+  end
+
+  it "should not accept the first input which is invalid but the second" do
+    type_game = "1\n"
+    first_player = "asdfjkl;df\n1\n"
+    input = StringIO.new("#{type_game}#{first_player}")
+    output = StringIO.new
+    interface = CliInterface.new(input, output)
+
+    game_play = GamePlay.new(Board.new, interface)
+
+    game_play.game_selection
+    game_play.select_first_player
+
+    expect(game_play.current_player).to eq(game_play.player_two)
   end
 end
