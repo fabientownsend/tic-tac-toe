cc04c3651122b4b6fe07fc57d59fe7a702f9d197
fabien townsend <fabientownsend@gmail.com>
2016-10-05 10:23:55 +0100

    add more integration tests

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Computer do
   let (:output) { StringIO.new }
   let (:board) { Board.new }
 
-  it "the computer will move to block its opponent" do
+  it "Computer block its opponent in a row" do
     board.string_to_board("OOX,XXO,   ")
 
     computer = Computer.new(Mark::ROUND, board)
@@ -17,12 +17,48 @@ RSpec.describe Computer do
     expect(board.to_s).to eq("OOX,XXO,O  ")
   end
 
-  it "the computer will move to win" do
+  it "Computer block its opponent in a diagonal" do
+    board.string_to_board("XO ,  O,  X")
+
+    computer = Computer.new(Mark::ROUND, board)
+
+    board.set_mark(computer.mark, computer.next_move)
+    expect(board.to_s).to eq("XO , OO,  X")
+  end
+
+  it "Computer block its opponent in a column" do
+    board.string_to_board("XO ,X O,   ")
+
+    computer = Computer.new(Mark::ROUND, board)
+
+    board.set_mark(computer.mark, computer.next_move)
+    expect(board.to_s).to eq("XO ,X O,O  ")
+  end
+
+  it "Computer move to win in a row" do
     board.string_to_board("XXO,OO ,   ")
 
     computer = Computer.new(Mark::ROUND, board)
 
     board.set_mark(computer.mark, computer.next_move)
     expect(board.to_s).to eq("XXO,OOO,   ")
+  end
+
+  it "Computer move to win in a row" do
+    board.string_to_board("XXO, XO,   ")
+
+    computer = Computer.new(Mark::ROUND, board)
+
+    board.set_mark(computer.mark, computer.next_move)
+    expect(board.to_s).to eq("XXO, XO,  O")
+  end
+
+  it "Computer move to win in a diagonal" do
+    board.string_to_board("XXO, OX,   ")
+
+    computer = Computer.new(Mark::ROUND, board)
+
+    board.set_mark(computer.mark, computer.next_move)
+    expect(board.to_s).to eq("XXO, OX,O  ")
   end
 end
